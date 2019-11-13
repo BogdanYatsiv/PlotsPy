1823c3d1516d3ecf4eefb5affcda2180cdb29ffb
BogdanYatsiv <bogdanyatsiv@gmail.com>
2019-11-13 19:48:56 +0200

    add main files

---
+++ my_plots.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import numpy as np
+
+#====PLOT====
+t = linspace(0, 3, 51)# 51 dot betwen 0 and 3
+y = t**2*exp(-t**2)
+
+plt.plot(t,y)
+plt.show()
+
+#====BATTERFLY PLOT====
+#x=sin(t)*(e^cos(t)-2cos(4t)+sin^5(1/12t))
+#y=cos(t)*(e^cos(t)-2cos(4t)+sin^5(1/12t))
+#t is[0;12π]
+
+t = linspace(0, 12*np.pi,500)
+x = sin(t)*(exp(cos(t)-2*cos(4*t)+sin(1/(12*t)**5)))
+y = cos(t)*(exp(cos(t)-2*cos(4*t)+sin(1/(12*t)**5)))
+
+plt.plot(x,y,"r--")
+
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Batterfly")
+#plt.legend()
+plt.grid()
+plt.show()
+
+#====LEMINISKATA BERNULI====
+theta = np.arange
