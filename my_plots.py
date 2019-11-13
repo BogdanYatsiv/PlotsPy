0422736b27fa2b0e9260ffd68a00e079d310993e
BogdanYatsiv <bogdanyatsiv@gmail.com>
2019-11-13 20:16:55 +0200

    repair plot prog

--- my_plots.py
+++ my_plots.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
-from Polynomial import *
+from Polynomial import Polinom
 
 #====PLOT====
-t = linspace(0, 3, 51)# 51 dot betwen 0 and 3
-y = t**2*exp(-t**2)
+t = np.linspace(0, 3, 51)# 51 dot betwen 0 and 3
+y = t**2*np.exp(-t**2)
 
 plt.plot(t,y)
 plt.show()
@@ -15,9 +15,9 @@ plt.show()
 #y=cos(t)*(e^cos(t)-2cos(4t)+sin^5(1/12t))
 #t is[0;12π]
 
-t = linspace(0, 12*np.pi,500)
-x = sin(t)*(exp(cos(t)-2*cos(4*t)+sin(1/(12*t)**5)))
-y = cos(t)*(exp(cos(t)-2*cos(4*t)+sin(1/(12*t)**5)))
+t = np.linspace(0, 12*np.pi,500)
+x = np.sin(t)*(np.exp(np.cos(t)-2*np.cos(4*t)+np.sin(1/(12*t)**5)))
+y = np.cos(t)*(np.exp(np.cos(t)-2*np.cos(4*t)+np.sin(1/(12*t)**5)))
 
 plt.plot(x,y,"r--")
 
@@ -30,7 +30,7 @@ plt.show()
 
 #====LEMINISKATA BERNULI====
 # theta = np.arange
-dots = linspace(0, 10, 10)
+dots = np.linspace(0, 10, 10)
 cont = [(23,5),(13,1),(1,2)]
 a = Polinom(cont,5)
 poly_y = a(dots,2)
